9e4ac70032c4f001d8fc8046d4146953a965d191
JesusAntonioGP <103076819+JesusAntonioGP@users.noreply.github.com>
2022-07-31 21:16:27 -0700

    Update seasonal-styles.js

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -3,5 +3,57 @@ $("document").ready(function(){
         e.preventDefault();//stop default submission
         var season = $(this).attr("href");//contents of href attribute of this element
         alert(season);
+
+
+// changes to spring link
+
+switch("#seasons"){
+
+
+    case 0: 
+        season = "spring";
+        $("header h3").text("Jump into spring! ");
+        $("html").css("background-color","#2B7129");
+        $("#wear").attr("src","images/spring-wear.jpg");
+        $("#logo").attr("src","images/spring.gif");
+        break;
+
+    case 1:
+        season = "summer"
+        $("header h3").text("Jump into Summer! ");
+        $("html").css("background-color","#EBA52B");
+        $("#wear").attr("src","images/summer-wear.jpg");
+        $("#logo").attr("src","images/summer.gif");
+        break;
+
+    case 2:
+
+    season = "fall"
+        $("header h3").text("Jump into Fall! ");
+        $("html").css("background-color","#A81124");
+        $("#wear").attr("src","images/fall-wear.jpg");
+        $("#logo").attr("src","images/fall.gif");
+        break;
+
+    case 3: 
+
+    season = "winter"
+
+        $("header h3").text("Jump into Winter! ");
+
+        $("html").css("background-color","#005393");
+        $("#wear").attr("src","images/winter-wear.jpg");
+        $("#logo").attr("src","images/winter.gif");
+
+}
+       
+
+       
+
+        
+
+
+
     });
 });
+
